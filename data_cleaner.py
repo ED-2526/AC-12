098f0a01e2b1b8d0f179e521efb901373f496e99
laurabuca <1710559@uab.cat>
2025-11-25 17:23:48 +0100

    Add files via upload

--- data_cleaner.py
+++ data_cleaner.py
@@ -11,34 +11,34 @@ def load_and_clean(path, min_ratings_per_user=20, min_ratings_per_item=100, frac
         fraction (float): si és diferent de None, agafar només una fracció aleatòria del dataset.
         seed (int): random seed per sampling.
     """
-    # El CSV NO té header → li posem nosaltres els noms correctes
     df = pd.read_csv(
         path,
         header=None,
         names=["userID", "itemID", "rating", "timestamp"]
     )
 
-    # Eliminar files amb valors nuls
     df = df.dropna(subset=['userID', 'itemID', 'rating'])
 
-    # Convertir a tipus adequats
     df['userID'] = df['userID'].astype(str)
     df['itemID'] = df['itemID'].astype(str)
     df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
-
-    # Eliminar files on rating no sigui número
     df = df.dropna(subset=['rating'])
 
-    # Convertir timestamp → opcional
     df['timestamp'] = pd.to_numeric(df['timestamp'], errors='coerce')
 
-    # Filtrar usuaris amb poques valoracions
+    # TRACTAR DUPLICATS: (userID, itemID): fem la mitjana
+    df = df.groupby(["userID", "itemID"], as_index=False).agg({
+        "rating": "mean",
+        "timestamp": "max"
+    })
+
+    # Filtrar usuaris amb poques valoracions totals
     df = df.groupby("userID").filter(lambda x: len(x) >= min_ratings_per_user)
 
-    # Filtrar items amb poques valoracions
+    # Filtrar items amb poques valoracions totals
     df = df.groupby("itemID").filter(lambda x: len(x) >= min_ratings_per_item)
 
-    # Opcional: agafar només una fracció aleatòria
+    # Sampling opcional
     if fraction is not None:
         df = df.sample(frac=fraction, random_state=seed)
 
