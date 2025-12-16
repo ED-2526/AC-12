5fbf7e4260f6137e4a468fc5d7ba095c18b5f135
Lucia Rodriguez Arrieta <146718605+luchy33@users.noreply.github.com>
2025-12-16 01:19:42 +0100

    Update data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -12,27 +12,20 @@ def load_and_clean(path, min_ratings_per_user=20, min_ratings_per_item=100, frac
         fraction (float): si és True, agafar només una fracció aleatòria del dataset.
         seed (int): random seed per sampling.
     """
-    df = pd.read_csv(path, header=None, names=["userID", "itemID", "rating", "timestamp"]) #llegeix l'arxiu csv i posa nom a cada columna (no es veu)
-    #print(df.head(10)) 10 primeres del dataset sencer
-    df = df.dropna(subset=['userID', 'itemID', 'rating']) #eliminem les files on algun d'aquests camps sigui nul
-    df['userID'] = df['userID'].astype(str) #passem a string l'userID
-    df['itemID'] = df['itemID'].astype(str) #passem a string l'itemID
-    df['rating'] = pd.to_numeric(df['rating'], errors='coerce') #converteix ratings a numeric (int o float) i si no es pot es NaN
-    df = df.dropna(subset=['rating']) #Eliminem les files on sigui NaN
-    df['timestamp'] = pd.to_numeric(df['timestamp'], errors='coerce') #converteix timestap a numeric (int o float) i si no es pot es NaN
-    #DUPLICATS (userID, itemID): fem la mitjana
-    df = df.groupby(["userID", "itemID"], as_index=False).agg({"rating": "mean", "timestamp": "max"}) #agrupem mateix user-item, segueixen sent columnes, fem la mitjana del rating i el max timestamp
-    # Filtrar usuaris amb poques valoracions totals
+    df = pd.read_csv(path, header=None, names=["userID", "itemID", "rating", "timestamp"]) 
+    df = df.dropna(subset=['userID', 'itemID', 'rating']) 
+    df['userID'] = df['userID'].astype(str)
+    df['itemID'] = df['itemID'].astype(str)
+    df['rating'] = pd.to_numeric(df['rating'], errors='coerce') 
+    df = df.dropna(subset=['rating']) 
+    df['timestamp'] = pd.to_numeric(df['timestamp'], errors='coerce') 
+    df = df.groupby(["userID", "itemID"], as_index=False).agg({"rating": "mean", "timestamp": "max"}) #DUPLICATS: fem la mitjana
     df = df.groupby("userID").filter(lambda x: len(x) >= min_ratings_per_user)
-    # Filtrar items amb poques valoracions totals
     df = df.groupby("itemID").filter(lambda x: len(x) >= min_ratings_per_item)  
-    #Sampling opcional
     if fraction is not None:
         df = df.sample(frac=fraction, random_state=seed)
     df.to_csv("cleaned_data.csv", index=False)
     return df
-
-
 
 def visualize_dataset(df):
     """
@@ -42,14 +35,15 @@ def visualize_dataset(df):
         df (pandas.DataFrame): dataset netejat a visualitzar
     """
     print("INFORMACIÓ DEL DATASET NETEJAT")
-    print("\nPrimeres 10 files:") #del sample, ordenat alfabeticament
+    print("\nPrimeres 10 files: ") 
     print(df.head(10))
     print("\nMida del dataset netejat:", df.shape)
-    print("\nNombre d’usuaris:", df["userID"].nunique()) #selecciona la col usuaris i compta valors unics
+    print("\nNombre d’usuaris:", df["userID"].nunique()) 
     print("Nombre d’items:", df["itemID"].nunique())  
     #print("\nEstadístiques dels usuaris: "df["userID"].value_counts().describe())
     #print("\nEstadístiques dels items: "df["itemID"].value_counts().describe())
-    print("\nUsuaris més actius:", df["userID"].value_counts().head(10)) #de més gran a més petit
+    print("\nUsuaris més actius:", df["userID"].value_counts().head(10))
     print("\nItems més valorats:", df["itemID"].value_counts().head(10))
+
 
 
